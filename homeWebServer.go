938695ab542ca8e77f03602422d3b6915f8bb5c3
Vladislav Makashov <makashov73@gmail.com>
2023-10-22 20:10:23 +0400

    Ton of changes: add support of subfolders add table view of file list add styles for entire page

--- homeWebServer.go
+++ homeWebServer.go
@@ -9,38 +9,50 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
-func getIcon(ext string) string {
-	switch ext {
-	case "jpg", "png", "gif", "jpeg":
-		return "/assets/icons/images.png"
-	case "mp4", "avi", "mkv":
-		return "/assets/icons/videos.png"
+func formatDuration(duration time.Duration) string {
+	minutes := int(duration.Minutes())
+	return fmt.Sprintf("%d:%02d", minutes/60, minutes%60)
+}
+
+func formatSize(size int64) string {
+	const (
+		_ = 1 << (10 * iota)
+		KB
+		MB
+		GB
+	)
+	switch {
+	case size >= GB:
+		return fmt.Sprintf("%.2f GB", float64(size)/GB)
+	case size >= MB:
+		return fmt.Sprintf("%.2f MB", float64(size)/MB)
+	case size >= KB:
+		return fmt.Sprintf("%.2f KB", float64(size)/KB)
 	default:
-		return "/assets/icons/unknown.png"
+		return fmt.Sprintf("%d B", size)
 	}
 }
 
-func listFiles(w http.ResponseWriter, r *http.Request) {
-	basePath, _ := os.Getwd() // Get the current working directory
-	files, err := filepath.Glob(filepath.Join(basePath, "files", "*"))
+func listFiles(dirPath string, w http.ResponseWriter, r *http.Request, indent string) {
+	files, err := ioutil.ReadDir(dirPath)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	for _, file := range files {
-		relPath, _ := filepath.Rel(basePath, file)
+		relPath := filepath.Join(dirPath, file.Name())
 
-		// Check if the file is .DS_Store
-		if filepath.Base(relPath) == ".DS_Store" {
-			continue
-		}
-
-		// Check if the path is a directory
-		if info, err := os.Stat(file); err == nil && !info.IsDir() {
-			buf, err := ioutil.ReadFile(file)
+		if file.IsDir() {
+			fmt.Fprintf(w, "<tr>")
+			fmt.Fprintf(w, "<td colspan=\"4\" style=\"text-decoration: none; color:#747474; font-size: 24px; font-family: Arial;\">📁 %s%s/</td>", indent, file.Name())
+			fmt.Fprintf(w, "</tr>")
+			listFiles(relPath, w, r, indent+"  ")
+		} else if filepath.Base(relPath) != ".DS_Store" {
+			buf, err := ioutil.ReadFile(relPath)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
@@ -52,19 +64,54 @@ func listFiles(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 
-			fmt.Fprintf(w, "<html><head><title>mkv73's Media Server</title></head><body>")
-			fmt.Fprintf(w, "<a href=\"/%s\" style=\"text-decoration: none;\"><img src=\"%s\"> %s</a><br>", relPath, getIcon(kind.Extension), strings.TrimSuffix(filepath.Base(file), filepath.Ext(file)))
-			fmt.Fprintf(w, "</body></html>")
-			log.Println(relPath)
+			// Get the relative path of the file
+			relativePath, err := filepath.Rel(filepath.Join(os.Getenv("PWD"), "files"), relPath)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			// Replace backslashes with forward slashes for URL compatibility
+			relativePath = strings.ReplaceAll(relativePath, `\`, `/`)
+
+			// Get duration for video files
+			duration := ""
+			if kind.MIME.Type == "video" {
+				duration = formatDuration(time.Minute * 5) // Replace with actual duration calculation
+			}
+
+			// Get file size
+			fileInfo, err := os.Stat(relPath)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			size := formatSize(fileInfo.Size())
+
+			extension := filepath.Ext(file.Name())
+
+			fmt.Fprintf(w, "<tr>")
+			fmt.Fprintf(w, "<td>%s<a href=\"%s\" style=\"text-decoration: none; color:#F7F7F7; font-size: 30px; font-family: Arial;\">📀️ %s</a></td>", indent, relativePath, file.Name())
+			fmt.Fprintf(w, "<td align=\"center\" style=\"text-decoration: none; color:#F7F7F7; font-size: 22px; font-family: Arial;\">%s</a></td>", extension)
+			fmt.Fprintf(w, "<td align=\"center\" style=\"text-decoration: none; color:#F7F7F7; font-size: 22px; font-family: Arial;\">%s</td>", duration)
+			fmt.Fprintf(w, "<td align=\"center\" style=\"text-decoration: none; color:#F7F7F7; font-size: 22px; font-family: Arial;\">%s</td>", size)
+			fmt.Fprintf(w, "</tr>")
 		}
 	}
 }
 
 func main() {
-	basePath, _ := os.Getwd() // Get the current working directory
+	basePath, _ := os.Getwd()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.FileServer(http.Dir(filepath.Join(basePath, "./"))).ServeHTTP(w, r)
+		http.FileServer(http.Dir(filepath.Join(basePath, "files"))).ServeHTTP(w, r)
 	})
-	http.HandleFunc("/index", listFiles)
-	log.Fatal(http.ListenAndServe("localhost:8081", nil))
+	http.HandleFunc("/files", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<html><head><title>Home Media Server</title></head><body bgcolor=\"#494949\">")
+		fmt.Fprintf(w, "<h1 align=\"center\" style=\"color:#ABABAB;\">Home Media Server</h1>")
+		fmt.Fprintf(w, "<table width=\"100%\"><tr><th style=\"text-decoration: none; color:#9A9A9A; font-size: 18px; font-family: Arial;\">Name</th><th style=\"text-decoration: none; color:#9A9A9A; font-size:18px; font-family: Arial;\">Extension</th><th style=\"text-decoration: none; color:#9A9A9A; font-size: 18px; font-family: Arial;\">Durtaion</th><th style=\"text-decoration: none; color:#9A9A9A; font-size: 18px; font-family: Arial;\">Size</th></tr>")
+		listFiles(filepath.Join(basePath, "files"), w, r, "")
+		fmt.Fprintf(w, "</table></body></html>")
+	})
+	log.Println("tiny Web Server Successfully started at port 8081")
+	log.Fatal(http.ListenAndServe("0.0.0.0:8081", nil))
 }
